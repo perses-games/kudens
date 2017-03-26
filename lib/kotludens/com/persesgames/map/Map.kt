c459bf6aabcddde5ab3dd342b1f8b367a236e9f3
rnentjes <rien@nentjes.com>
2017-03-26 12:13:36 +0200

    clean up repository

--- lib/kotludens/com/persesgames/map/Map.kt
+++
@@ -1,9 +0,0 @@
-package com.persesgames.map
-
-/**
- * Created by rnentjes on 22-7-16.
- */
-
-open class Map {
-
-}
--- lib/kotludens/com/persesgames/texture/Sprites.kt
+++
@@ -1,7 +0,0 @@
-package com.persesgames.texture
-
-/**
- * User: rnentjes
- * Date: 17-4-16
- * Time: 15:44
- */
